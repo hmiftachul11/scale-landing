5fb3a32d6103f7e24814cd752d2e06fa8967736f
hmiftachul11 <hmiftachul11@gmail.com>
2025-11-24 01:12:05 +0700

    push test

---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { IntegrationsMarquee } from "@/components/seland/IntegrationsMarquee"
+
+export default function Test() {
+    return (
+        <div className="min-h-screen bg-black">
+            <IntegrationsMarquee />
+        </div>
+    )
+}
